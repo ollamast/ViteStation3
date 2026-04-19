3ca3d77e0e804a2bc849e84c6e3776f598ebcd0f
Amin Gatta <amin.gatta@ollamast.com>
2026-04-19 13:30:57 +0400

    test use lib gh-pages for clean deployment

--- dist/shaders/flow_fragment.glsl.js
+++
@@ -1,9 +0,0 @@
-let ff = document.getElementById('FlowFragment');
-
-ff.text = `precision lowp float;
-	
-varying float alpha;
-
-void main() {
-    gl_FragColor = vec4( alpha, alpha, alpha, 1.0 );
-}`
